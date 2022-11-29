0a19aed91bcb0c46d991900b90d1959510067914
BASSANT-PC\Bassant Gamil <bassantgamil@gmail.com>
2022-11-29 21:43:12 +0200

    save the reminder to the local db

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -68,9 +68,7 @@ class SaveReminderFragment : BaseFragment() {
             if (_viewModel.validateEnteredData(reminderDataItem)) {
                 addGeofence(reminderDataItem)
             }
-//            TODO: use the user entered reminder details to:
-//             1) add a geofencing request
-//             2) save the reminder to the local db
+            _viewModel.validateAndSaveReminder(reminderDataItem)
         }
     }
 
