811f62aff0765a8e66427b7607a4e20165cae716
BASSANT-PC\Bassant Gamil <bassantgamil@gmail.com>
2022-11-30 01:03:30 +0200

    Validate on Data

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,55 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.opengl.Visibility
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.mockito.ArgumentMatchers.matches
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    val reminder = ReminderDataItem("Home", "Fav place", "Egy", 3.2132, 6.9076)
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var rmRep: FakeDataSource
+
+    //Subject under test
+    private lateinit var viewModel: SaveReminderViewModel
+
+    @Before
+    fun setupViewModel() {
+        rmRep = FakeDataSource()
+        viewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), rmRep)
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun validateEnteredData_EmptyTitleAndUpdateSnackBar() {
+        assertThat(viewModel.validateEnteredData(reminder)).isFalse()
+    }
 
 }
