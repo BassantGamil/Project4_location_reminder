8229c317c7d02528e7bc9afbb26a2f13de7d101f
BASSANT-PC\Bassant Gamil <bassantgamil@gmail.com>
2022-11-29 18:39:45 +0200

    Add Activity when user authenticated , send him to RemindersActivity

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -26,7 +27,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
         var googleSignInClient: GoogleSignInClient
 
-    findViewById<View>(R.id.logIn_email_button).setOnClickListener {
+        findViewById<View>(R.id.logIn_email_button).setOnClickListener {
             LogIn();
         }
         findViewById<View>(R.id.logIn_google_button).setOnClickListener {
@@ -38,11 +39,6 @@ class AuthenticationActivity : AppCompatActivity() {
             startActivityForResult(signInIntent, RC_SIGN_IN)
 
         }
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-
     }
 
 
@@ -71,18 +67,23 @@ class AuthenticationActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode != SIGN_IN_RESULT_CODE) {
             Toast.makeText(this, getString(R.string.Login_failed), Toast.LENGTH_LONG).show()
             return
         }
-
         if (resultCode == RESULT_OK) {
             Toast.makeText(this, getString(R.string.Successful_login), Toast.LENGTH_LONG).show()
-
-
+            goToReminderActivity()
         }
     }
+
+    private fun goToReminderActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
+        return
+    }
+
     companion object {
         const val SIGN_IN_RESULT_CODE = 1001
         const val RC_SIGN_IN = 100
