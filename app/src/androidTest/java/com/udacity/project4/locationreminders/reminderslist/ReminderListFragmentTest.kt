71de67e7da734f37fa3855814b401db10a769bde
BASSANT-PC\Bassant Gamil <bassantgamil@gmail.com>
2022-12-01 07:28:02 +0200

    ReminderListFragmentTest.kt , ReminderDaoTest and RemindersLocalRepositoryTest use testcase for insert and get Data

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -6,13 +6,10 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -22,6 +19,8 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
@@ -29,9 +28,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
@@ -46,11 +43,13 @@ import org.mockito.Mockito.verify
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest : AutoCloseKoinTest() {
+    val reminder = ReminderDTO("Home", "Fav place", "Egy", 3.2132, 6.9076)
 
     @get:Rule
     private val instantExecutor = InstantTaskExecutorRule()
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private val dbBinding = DataBindingIdlingResource()
 
     @Before
     fun init() {
@@ -85,17 +84,25 @@ class ReminderListFragmentTest : AutoCloseKoinTest() {
         }
     }
 
-    private fun getReminder(): ReminderDTO {
-        return ReminderDTO("Home", "Fav place", "Egy", 3.2132, 6.9076)
+    @Test
+    fun clickFABToNavigateToSaveReminderFragment() {
+
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
     }
 
     @Test
-    fun onUI_noDataDisplayed() = runBlockingTest {
+    fun dataDisplayedOnUi() = runBlockingTest {
 
-        val reminder = getReminder()
-        runBlocking {
-            repository.saveReminder(reminder)
-        }
+        repository.saveReminder(reminder)
+
         launchFragmentInContainer<ReminderListFragment>(Bundle.EMPTY, R.style.AppTheme)
         onView(withText(reminder.title)).check(matches(isDisplayed()))
         onView(withText(reminder.description)).check(matches(isDisplayed()))
@@ -103,7 +110,7 @@ class ReminderListFragmentTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun check_save_button() {
+    fun checkSaveButton() {
         val scenario =
             launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
         val navController = mock(NavController::class.java)
@@ -116,4 +123,15 @@ class ReminderListFragmentTest : AutoCloseKoinTest() {
         verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
     }
 
+    @Test
+    fun errorMessageShowNoData() {
+        val scenario =
+            launchFragmentInContainer<ReminderListFragment>(Bundle.EMPTY, R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+
+        dbBinding.monitorFragment(scenario)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+
+        onView(withText("No Data")).check(matches(isDisplayed()))
+    }
 }
