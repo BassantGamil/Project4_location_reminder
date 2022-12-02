82cf9ba0ab2da0931343a91bdd9c3a228b006c6c
BASSANT-PC\Bassant Gamil <bassantgamil@gmail.com>
2022-12-02 11:20:47 +0200

    FakeDataSource.kt with documented comment in every method

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,32 +6,34 @@ import com.udacity.project4.locationreminders.data.dto.Result
 class FakeDataSource(
     private val reminders: MutableList<ReminderDTO> = mutableListOf()
 ) : ReminderDataSource {
-var isReturnError = false
-    fun setIsdReturnError(_isReturnError:Boolean){
-        this.isReturnError=_isReturnError
-    }
+    var isReturnError = false
 
+    //get list of data after check on this if success or failed
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        return if(isReturnError){
-            Result.Error("Error Retrieving Data " , 404)
-        }
-        else{
+        return if (isReturnError) {
+            Result.Error("Error Retrieving Data ", 404)
+        } else {
             Result.Success(ArrayList(reminders))
         }
     }
 
+    //save data in mutable list of reminder
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("Not yet implemented")
+        reminders.add(reminder)
     }
 
+    //get item of reminder by id
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         return if (isReturnError) {
-            Result.Error("Error")
+            //If there is Exception.
+            Result.Error("Error Exception")
         } else {
             val reminder = reminders.find { it.id == id }
             if (reminder == null) {
+                //Error to get Data , not found this
                 Result.Error("Not found")
             } else {
+                //Get Data Successfully
                 Result.Success(reminder)
             }
         }
@@ -39,6 +41,7 @@ var isReturnError = false
 
 
     override suspend fun deleteAllReminders() {
+        //Clear all reminders data
         reminders.clear()
     }
 }
